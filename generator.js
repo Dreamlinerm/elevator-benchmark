c958eed9bd7475f6bf59eb499c77ec04fb58ac61
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-12-01 22:36:23 +0100

    js

--- generator.js
+++ generator.js
@@ -2,12 +2,30 @@ const fs = require("fs");
 const { randomInt } = require("crypto");
 
 const instructions = ["u", "d", "o", "c"];
-const characterAmount = 1_000_000_000;
+const characterAmount = 1_00_000_000;
+const chunkSize = 10_000_000; // Write in chunks of 10 million characters
 
 const writeStream = fs.createWriteStream("input.txt");
 
-for (let i = 0; i < characterAmount; i++) {
-  writeStream.write(instructions[randomInt(4)]);
+let written = 0;
+
+function writeChunk() {
+  let chunk = "";
+  for (let i = 0; i < chunkSize; i++) {
+    chunk += instructions[randomInt(4)];
+  }
+  return writeStream.write(chunk);
 }
 
-writeStream.end();
+function write() {
+  while (written < characterAmount) {
+    if (!writeChunk()) {
+      writeStream.once("drain", write);
+      return;
+    }
+    written += chunkSize;
+  }
+  writeStream.end();
+}
+
+write();
